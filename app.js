b9670f901932267c94d7fc561837270a1e81aa59
dongsu <sonicce99@naver.com>
2022-03-27 15:45:58 +0900

    docs : Vanila Javascript 연습

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const XHR = new XMLHttpRequest();
 
 const newsURL = "https://api.hnpwa.com/v0/news/1.json";
+const contentURL = "https://api.hnpwa.com/v0/item/:id.json";
 
 XHR.open("GET", newsURL, false);
 
@@ -10,12 +11,33 @@ const newsFeed = JSON.parse(XHR.response);
 
 console.log(newsFeed);
 
+window.addEventListener("hashchange", () => {
+  const id = location.hash.slice(1);
+
+  XHR.open("GET", contentURL.replace(/:id/g, id), false);
+  XHR.send();
+
+  const newsContent = JSON.parse(XHR.response);
+
+  console.log(newsContent);
+
+  if (newsContent) {
+    root.innerHTML += `
+      <div>${newsContent.title}</div>
+    `;
+  }
+});
+
 for (let i = 0; i < 10; i++) {
   const root = document.getElementById("root");
 
   root.innerHTML += `
   <ul>
-    <li>${newsFeed[i].title}</li>
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
   </ul>
   `;
 }
